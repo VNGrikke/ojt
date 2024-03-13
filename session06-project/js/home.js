b201657c6abac09fdc21f379a90a8425bd965ace
VNGrikke <Enlove2005@gmail.com>
2024-03-13 08:51:24 +0700

    Add

--- session06-project/js/home.js
+++ session06-project/js/home.js
@@ -3,7 +3,6 @@ let check = false;
 let images = JSON.parse(localStorage.getItem("images"))||[];
     // localStorage.setItem("images",JSON.stringify(images));
 function change() {
-    console.log(account);
     if (account !== null && typeof account === 'object' && Object.keys(account).length == 0) {
         check = false;
         document.getElementById("user").innerHTML = `<div id="user"> <a href="/session06-project/page/register.html">Đăng kí</a>/<a href="/session06-project/page/login.html">Đăng nhập</a></div>`
@@ -31,9 +30,9 @@ function render() {
        products += `
                     <a href="">
                         <div class="product">
-                        <div class="productImg"><img src="${images[i]}"></div>
-                        <div class="productName">Gối ôm Dakimakura</div>
-                        <div class="price">120.000VNĐ</div>
+                        <div class="productImg"><img src="git remote add Origin https://github.com/VNGrikke/ojt.git${images[i].src}"></div>
+                        <div class="productName">${images[i].name}</div>
+                        <div class="price">${images[i].price}</div>
                         <button>Mua</button>
                     </div>
                     </a>
